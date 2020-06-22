9ae7025ed97aebacce8b4fe60162c8a28bf876b9
Sunny Johal <sunny@titaniumthemes.com>
2020-06-22 19:30:22 +0100

    Removed bin scripts and added a passing js unit test

--- test/unit/my-test.test.js
+++ test/unit/my-test.test.js
@@ -1,5 +1,5 @@
 describe('This is a test', () => {
-  test('Example test that should fail', () => {
-    expect(false).toBeTruthy();
+  test('Example test that should pass to test travisci', () => {
+    expect(true).toBeTruthy();
   });
 });
