b17e997d3307ca7d85932e511c8ed1ad3871345c
Sunny Johal <sunny@titaniumthemes.com>
2020-11-15 13:15:20 +0000

    refactor: simplified customizer integration

--- src/includes/customizer.php
+++ src/includes/customizer.php
@@ -11,7 +11,6 @@
 
 namespace ECS\Customizer;
 
-use ECS\Data;
 use ECS\Frontend;
 
 /**
@@ -26,23 +25,14 @@ function prepare_sidebars_for_customizer() {
 		return;
 	}
 
-	if ( ! Frontend\sidebar_replacements_determined() ) {
-		$default_widget_area_ids = wp_list_pluck( Data\get_default_registered_sidebars(), 'id' );
-		Frontend\determine_sidebar_replacements( $default_widget_area_ids );
-	}
-
 	$all_replacements = Frontend\get_all_sidebar_replacements();
 
-	if ( ! empty( $all_replacements ) ) {
-		ob_start();
+	ob_start();
 
-		// Call dynamic_sidebar() in the output buffer so
-		// that the customizer detects the replacement.
-		foreach ( $all_replacements as $id => $replacement_id ) {
-			dynamic_sidebar( $replacement_id );
-		}
-
-		ob_end_clean();
+	foreach ( $all_replacements as $id => $replacement_id ) {
+		dynamic_sidebar( $replacement_id );
 	}
+
+	ob_end_clean();
 }
 add_action( 'wp_footer', __NAMESPACE__ . '\\prepare_sidebars_for_customizer' );
